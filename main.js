81c40a06f725bfd80291733b599e35d76b896de2
MonroyA2018 <amonroy@sjnma.org>
2018-05-17 09:16:33 -0500

    Hello

---
+++ main.js
@@ -0,0 +1,34 @@
+alert("hello!");
+alert("do you like pizza");
+alert("pizza is great");
+console.log("but gelato is ok");
+ 
+
+alert("You are now reading fun story!");
+
+ 
+
+var noun1 = prompt("please enter a noun.", "");
+
+var verb1 = prompt("please enter a verb.", "");
+
+var noun2 = prompt("please enter a noun.", "");
+
+var verb2 = prompt("please enter a verb.", "");
+
+var noun4 = prompt("please enter a noun.", "");
+
+var adj1 = prompt("please enter an adjcive.", "");
+
+var verb3 = prompt("please enter a verb.", "");
+
+var verb4 = prompt("please enter a verb.", "");
+
+var adj2 = prompt("please enter an adjcive.", "");
+
+var noun4 = prompt("please enter a noun.", "");
+
+ 
+
+document.getElementById("name").innerHTML = "the " + noun1 + " needed to hurry so he " + verb1 + " he then went to the " + noun2 + " . After that he " + verb2 + " and he was " + adj1 + " so he started " + verb3 + " this led to " + verb4 + " he then was " + adj2 + " he then ran again. " + "The end! "; 
+
